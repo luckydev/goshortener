3312e8df610b48e2cd23953dd323111bc5621840
lakshmanan <lucky.developer@gmail.com>
2011-02-05 01:23:21 +0530

    initial shorten and lengthen functions

---
+++ lib/goshortener.rb
@@ -0,0 +1,38 @@
+require "rubygems"
+require "bundler/setup"
+
+require "rest_client"
+require "json"
+
+
+class Go
+
+  def initialize
+    @base_url = "https://www.googleapis.com/urlshortener/v1/url"
+  end
+
+  def shorten(long_url)
+    if long_url.is_a? String
+      request_json = {'longUrl' => long_url}.to_json
+      response = RestClient.post @base_url, request_json, :accept => :json, :content_type => :json
+    else
+      raise "Please provide a valid Long url string"
+    end
+    response = JSON.parse response
+    short_url = response["id"]
+    return short_url
+  end
+
+  def lengthen(short_url)
+    if short_url.is_a? String
+      request_params = {:shortUrl => short_url}
+      response = RestClient.get @base_url, :params => request_params
+    else
+      raise "Please provide a valid short url string"
+    end
+    response = JSON.parse response
+    long_url = response["longUrl"]
+    return long_url
+  end
+
+end 
