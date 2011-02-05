4adadca3de24b1d6cf533a0353b2c6544b1c55fa
Waseem Ahmad <talk.to.waseem@gmail.com>
2011-02-05 18:15:39 +0530

    Accept only valid URLs while lengthening. Some invalid URLs are still accepted.

--- lib/goshortener.rb
+++ lib/goshortener.rb
@@ -24,7 +24,23 @@ class GoShortener
   end
 
   def lengthen(short_url)
-    if short_url.is_a? String
+    ##
+    # Accepts a short url shortened by http://goo.gl and returns original
+    # long url.
+    # 
+    # Examples:
+    # go = GoShortener.new
+    #
+    # go.lengthen("http://goo.gl/TCZHi") #=> "http://github.com/luckydev"
+    #
+    # go.lengthen("http://www.goo.gl/TCZHi") #=> Error
+    # go.lengthen("goo.gl/TCZHi")        #=> Error
+    # go.lengthen("http://goo.gl/")      #=> Error
+    # go.lengthen("http://goo.gl/T")     #=> RestClient::BadRequest: 400 Bad Request
+    # go.lengthen("http://bit.ly/TCZHi") #=> Error
+    #
+    ##
+    if short_url.is_a?(String) && short_url =~ /^http:\/\/goo\.gl\/.+$/
       request_params = {:shortUrl => short_url}
       response = RestClient.get @base_url, :params => request_params
     else
